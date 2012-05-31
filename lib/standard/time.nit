a85bb8c6ae18d4f146ad3479717d14b0a5021f3a
Alexis Laferrière <alexis.laf@xymus.net>
2012-05-30 20:01:47 -0400

    lib: adds sleep methods to Int in the time module
    Signed-off-by: Alexis Laferrière <alexis.laf@xymus.net>

--- lib/standard/time.nit
+++ lib/standard/time.nit
@@ -1,10 +1,11 @@
 # This file is part of NIT ( http://www.nitlanguage.org ).
 #
 # Copyright 2008 Floréal Morandat <morandat@lirmm.fr>
+# Copyright 2012 Alexis Laferrière <alexis.laf@xymus.net>
 #
 # This file is free software, which comes along with NIT.  This software is
 # distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-# without  even  the implied warranty of  MERCHANTABILITY or  FITNESS FOR A 
+# without  even  the implied warranty of  MERCHANTABILITY or  FITNESS FOR A
 # PARTICULAR PURPOSE.  You can modify it is you want,  provided this header
 # is kept unaltered, and a notification of the changes is added.
 # You  are  allowed  to  redistribute it and sell it, alone or is a part of
@@ -18,4 +19,12 @@ import kernel
 redef class Object
 	# Unix time: the number of seconds elapsed since January 1, 1970
 	protected fun get_time: Int is extern "kernel_Any_Any_get_time_0"
+end
+
+redef class Int
+	# Sleep for a specified amount of time in seconds
+	fun sleep is extern
+
+	# Sleep for a specified amount of time in nanoseconds
+	fun nanosleep is extern
 end
--- lib/standard/time_nit.c
+++ lib/standard/time_nit.c
@@ -1,25 +1,34 @@
-#ifndef __TIME_NIT_H
-#define __TIME_NIT_H
 /* This file is part of NIT ( http://www.nitlanguage.org ).
  *
- * Copyright 2008 Floréal Morandat <morandat@lirmm.fr> 
  * Copyright 2012 Alexis Laferrière <alexis.laf@xymus.net>
  *
  * This file is free software, which comes along with NIT.  This software is
  * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without  even  the implied warranty of  MERCHANTABILITY or  FITNESS FOR A 
+ * without  even  the implied warranty of  MERCHANTABILITY or  FITNESS FOR A
  * PARTICULAR PURPOSE.  You can modify it is you want,  provided this header
  * is kept unaltered, and a notification of the changes is added.
  * You  are  allowed  to  redistribute it and sell it, alone or is a part of
  * another product.
  */
 
-#include <time.h>
-#define kernel_Any_Any_get_time_0(self) time(NULL)
+#include "time_nit.h"
 
-#include <time._nitni.h>
-
-void Int_sleep___impl( bigint recv );
-void Int_nanosleep___impl( bigint recv );
-
+#ifdef _POSIX_C_SOURCE
+	#undef _POSIX_C_SOURCE
 #endif
+#define _POSIX_C_SOURCE 199309L
+#include <time.h>
+
+#include <unistd.h>
+
+void Int_sleep___impl( bigint recv )
+{
+	sleep( recv );
+}
+
+void Int_nanosleep___impl( bigint recv )
+{
+	struct timespec req = {recv/1000000000L,recv%1000000000L};
+	nanosleep( &req, NULL );
+}
+
